e0be08e6aac0d9d40f1b51753a86900d929264d3
Prashant_Gaikwad_701 <165748489+Pbg701@users.noreply.github.com>
2024-09-23 12:35:17 +0530

    commit

--- bicycle_app/lib/main.dart
+++ bicycle_app/lib/main.dart
@@ -1,22 +1,5 @@
-// import 'package:flutter/material.dart';
 
-// import 'View/get_started_page.dart';
 
-// void main() {
-//   runApp(const MainApp());
-// }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: GetStartedScreen(),
-//     );
-//   }
-// }
 import 'package:bicycle_app/View/GetStartedScreen/get_started_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
